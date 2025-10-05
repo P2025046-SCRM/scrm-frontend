0d655ed7d21f28dec85c9ec7e6b6f3a21c3ea357
Mario Valdivieso <g10vann1vch@gmail.com>
2025-10-04 22:18:37 -0500

    feat: add signup screen - add signup screen widget - add signup screen to routes file

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:scrm/presentation/login/login_screen.dart';
+import 'package:scrm/presentation/signup/signup_screen.dart';
 
 class AppRoutes {
-  static String initialRoute = 'login';
+  static String initialRoute = 'signup';
   static Map<String, Widget Function(BuildContext)> getRoutes = {
     'login': (BuildContext context) => LoginScreen(),
+    'signup': (BuildContext context) => SignupScreen(),
   };
 }
