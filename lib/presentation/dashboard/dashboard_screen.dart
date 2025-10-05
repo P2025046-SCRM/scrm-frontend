ffa7dc99660397912fa1e48901e9a3e990f66bba
Mario Valdivieso <g10vann1vch@gmail.com>
2025-10-05 12:02:47 -0500

    feat: add scrollview and bottom navbar to dashboard screen widget

--- lib/presentation/dashboard/dashboard_screen.dart
+++ lib/presentation/dashboard/dashboard_screen.dart
@@ -29,48 +29,82 @@ class _DashboardScreenState extends State<DashboardScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Hola, Juan Perez', style: kSubtitleTextStyle,),
-            SizedBox(height: 20,),
-            Container(
-              height: 200,
-              width: double.infinity,
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.green[100],
-                borderRadius: BorderRadius.circular(8),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Hola, Juan Perez', style: kSubtitleTextStyle,),
+              SizedBox(height: 20,),
+              Container(
+                height: 200,
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.green[100],
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
-            ),
-            Text('Porcentaje de Materiales Reutilizables', style: kRegularTextStyle,),
-            Text('Reciclable: 80%    |    No Reciclable: 20%', style: kDescriptionTextStyle,),
-            SizedBox(height: 25,),
-            Container(
-              height: 200,
-              width: double.infinity,
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.green[100],
-                borderRadius: BorderRadius.circular(8),
+              Text('Porcentaje de Materiales Reutilizables', style: kRegularTextStyle,),
+              Text('Reciclable: 80%    |    No Reciclable: 20%', style: kDescriptionTextStyle,),
+              SizedBox(height: 25,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Text('1240', style: kTitleTextStyle,),
+                      SizedBox(height: 2,),
+                      Text('Unidades de Residuos Procesadas', style: kRegularTextStyle,),
+                    ],
+                  )
+                ],
               ),
-            ),
-            Text('Porcentaje de Materiales Reutilizables', style: kRegularTextStyle,),
-            Text('Retazos: 40% | Biomasa: 30% | Metales: 10% | Piezas Plásticas: 5%', style: kDescriptionTextStyle,),
-            SizedBox(height: 25,),
-            Container(
-              height: 200,
-              width: double.infinity,
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.green[100],
-                borderRadius: BorderRadius.circular(8),
+              SizedBox(height: 25,),
+              Container(
+                height: 200,
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.green[100],
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
-            ),
-            Text('Porcentaje de Materiales Reutilizables', style: kRegularTextStyle,),
-            Text('Alto (91.25%)', style: kDescriptionTextStyle,),
-          ],
+              Text('Residuos Reutilizables por Tipo', style: kRegularTextStyle,),
+              Text('Retazos: 40% | Biomasa: 30% | Metales: 10% | Piezas Plásticas: 5%', style: kDescriptionTextStyle,),
+              SizedBox(height: 25,),
+              Container(
+                height: 200,
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.green[100],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              Text('Porcentaje de Aciertos', style: kRegularTextStyle,),
+              Text('Alto (91.25%)', style: kDescriptionTextStyle,),
+            ],
+          ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt_outlined),
+            label: 'Clasificar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Historial',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.green,
+        onTap: (index) { },
       ),
     );
   }
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -6,7 +6,7 @@ import '../presentation/profile/edit_profile_screen.dart';
 import '../presentation/profile/profile_screen.dart';
 
 class AppRoutes {
-  static String initialRoute = 'login';
+  static String initialRoute = 'dashboard';
   static Map<String, Widget Function(BuildContext)> getRoutes = {
     'login': (BuildContext context) => LoginScreen(),
     'signup': (BuildContext context) => SignupScreen(),
