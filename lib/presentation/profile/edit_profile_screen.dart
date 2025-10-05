ee95fcf039aabcf94b79bcfe37cb7ac20321d1a7
Mario Valdivieso <g10vann1vch@gmail.com>
2025-10-05 01:01:52 -0500

    feat: add edit profile screen widget - update login screen to use 'Email' label - add edit profile screen

---
+++ lib/presentation/profile/edit_profile_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:scrm/common/widgets/text_field_widget.dart';
+import '../../common/styles/text_styles.dart';
+import '../../common/widgets/hl_button_widget.dart';
+
+class EditProfileScreen extends StatefulWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Editar Perfil', style: kTitleTextStyle,),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 12,),
+            TextFieldWidget(textController: nameController, text: 'Nombre'),
+            SizedBox(height: 12,),
+            TextFieldWidget(textController: emailController, text: 'Email'),
+            Spacer(),
+            SizedBox(
+              height: 48,
+              width: double.infinity,
+              child: HighlightedButton(buttonText: 'Guardar Cambios', onPressed: (){},),
+            ),
+            SizedBox(height: 12,),
+          ],
+        ),),
+      ),
+    );
+  }
+}
