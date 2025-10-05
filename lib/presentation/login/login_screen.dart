9d3649e163fca7321c43d32a083878e8a5e6a2dd
Mario Valdivieso <g10vann1vch@gmail.com>
2025-10-04 20:27:24 -0500

    feat: add login screen style - add login button - add 'forgot password' and 'create account' text buttons - add complete login screen style

--- lib/presentation/login/login_screen.dart
+++ lib/presentation/login/login_screen.dart
@@ -21,23 +21,110 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:[
+          SizedBox(height: 40,),
+          Padding(
+          padding: const EdgeInsets.all(15.0),
           child: Form(child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Login'),
+              Text('Bienvenido', style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Inter',
+              ),),
+              SizedBox(height: 24,),
               TextFormField(
                 keyboardType: TextInputType.emailAddress,
                 controller: emailController,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                ),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Usuario',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
               ),
+              SizedBox(height: 12,),
               TextFormField(
                 keyboardType: TextInputType.visiblePassword,
                 controller: passwordController,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                ),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Contraseña',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
               ),
+              SizedBox(height: 24,),
+              SizedBox(
+                height: 48,
+                width: double.infinity,
+                child: ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightGreen,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )
+                  ),
+                  child: Text('Iniciar Sesión', style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Inter',
+                  ),),),
+              ),
+              SizedBox(height: 16,),
+              Center(
+                child: Column(
+                  children: [
+                    TextButton(onPressed: (){},
+                      child: Text('Olvidé mi contraseña',
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontSize: 14,
+                          color: Color.fromARGB(255, 99, 135, 99),
+                        ),)),
+                    SizedBox(height: 8,),
+                    TextButton(onPressed: (){},
+                      child: Text('Crear una cuenta',
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontSize: 14,
+                          color: Color.fromARGB(255, 99, 135, 99),
+                        ),)),
+                  ],
+                ),
+              )
             ],
-          )),),
+          )),),],
       ),
     );
   }
