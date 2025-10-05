580dcbc5423f4d225ae2f84e136b49c4ccba401f
Mario Valdivieso <g10vann1vch@gmail.com>
2025-10-04 21:33:30 -0500

    refactor: extract styled text fields and buttons to common widgets - add highlighted button widget - add text field widget - refactor login screen to reduce redundant lines - add global constants for custom text styles

---
+++ lib/common/styles/text_styles.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'Inter',
+);
+
+const kRegularTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 16,
+  fontFamily: 'Inter',
+);
+
+const kHintTextStyle = TextStyle(
+  color: Color(0xFF757575),
+  fontSize: 16,
+  fontFamily: 'Inter',
+);
+
+const kAltRegularTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'Inter',
+);
+
+const kTextButtonStyle = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 14,
+  color: Color.fromARGB(255, 99, 135, 99),
+);
--- lib/presentation/login/login_screen.dart
+++ lib/presentation/login/login_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:scrm/common/widgets/hl_button_widget.dart';
+
+import '../../common/styles/text_styles.dart';
+import '../../common/widgets/text_field_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -30,76 +34,16 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Form(child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Bienvenido', style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Inter',
-              ),),
+              Text('Bienvenido', style: kTitleTextStyle,),
               SizedBox(height: 24,),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                controller: emailController,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'Inter',
-                ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Usuario',
-                  hintStyle: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 16,
-                    fontFamily: 'Inter',
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
+              TextFieldWidget(textController: emailController, text: 'Usuario'),
               SizedBox(height: 12,),
-              TextFormField(
-                keyboardType: TextInputType.visiblePassword,
-                controller: passwordController,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'Inter',
-                ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Contraseña',
-                  hintStyle: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 16,
-                    fontFamily: 'Inter',
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
+              TextFieldWidget(textController: passwordController, text: 'Contraseña'),
               SizedBox(height: 24,),
               SizedBox(
                 height: 48,
                 width: double.infinity,
-                child: ElevatedButton(onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.lightGreen,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    )
-                  ),
-                  child: Text('Iniciar Sesión', style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Inter',
-                  ),),),
+                child: HighlightedButton(buttonText: 'Iniciar Sesión', onPressed: (){},),
               ),
               SizedBox(height: 16,),
               Center(
@@ -107,19 +51,11 @@ class _LoginScreenState extends State<LoginScreen> {
                   children: [
                     TextButton(onPressed: (){},
                       child: Text('Olvidé mi contraseña',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 14,
-                          color: Color.fromARGB(255, 99, 135, 99),
-                        ),)),
+                        style: kTextButtonStyle,)),
                     SizedBox(height: 8,),
                     TextButton(onPressed: (){},
                       child: Text('Crear una cuenta',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 14,
-                          color: Color.fromARGB(255, 99, 135, 99),
-                        ),)),
+                        style: kTextButtonStyle,)),
                   ],
                 ),
               )
